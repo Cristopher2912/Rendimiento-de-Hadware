794f3da955fde7b8eaf2540f221329b876cb7fad
Cristopher2912 <149955652+Cristopher2912@users.noreply.github.com>
2023-11-29 12:24:09 -0500

    Update rendimiento.py

--- rendimiento.py
+++ rendimiento.py
@@ -8,3 +8,13 @@ def obtener_rendimiento_cpu():
 while True:
     obtener_rendimiento_cpu()
     time.sleep(1)
+
+import subprocess
+
+def ping_ip(ip_address):
+	command = ['ping', '-c', '4', ip_address]
+	result = subprocess.run(command, capture_output=True, text=True)
+	print(result.stdout)
+
+ip_address = '10.3.21.194'
+ping_ip(ip_address)
